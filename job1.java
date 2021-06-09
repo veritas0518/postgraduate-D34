cf6118db2b8cf37dd614efa818fb7aace91e7a05
TianXing Xue <76930817+veritas0518@users.noreply.github.com>
2021-06-09 10:54:49 +0800

    Find the root of the quadratic equation
    【问题描述】编写程序，从键盘输入三个系数a，b和c，计算1.png的解并输出。需要考虑各种情况，包括方程有虚根、方程有实根、方程是一元一次方程、没有根等。输出的所有浮点数值保留6位小数，输入输出格式如样例所示。
    【样例输入1】1 3 2
    【样例输出1】The equation has real roots, x1 = -1.000000,  x2 = -2.000000
    
    【样例输入2】1 2 3
    【样例输出2】The equation has complex roots, x1 = -1.000000 + 1.414214i,  x2 = -1.000000 - 1.414214i
    
    【样例输入3】1 4 4
    【样例输出3】The equation has real roots, x1 = -2.000000,  x2 = -2.000000
    
    【样例输入4】0 0 0
    【样例输出4】There are infinite solutions.
    
    【样例输入5】0 0 8
    【样例输出5】The equation has no solution.

---
+++ job1.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+import java.util.Scanner;
+import java.math.*;
+
+import static java.lang.Math.sqrt;
+
+public class job1 {
+    public static void main(String[] args) {
+        double a,b,c,realPart,imgPart;
+        double x1,x2;
+        double q;
+        Scanner s = new Scanner(System.in);
+        a=s.nextDouble();b=s.nextDouble();c=s.nextDouble();
+        q=b*b-4*a*c;
+
+        if  (a==0&&b==0&&c==0){
+            System.out.println("There are infinite solutions.");
+
+        }
+        else if (a==0&&b==0&&c!=0){
+            System.out.println("The equation has no solution.");
+        }
+
+
+
+        else if (q>=0) {
+            x1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
+            x2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
+            System.out.printf("The equation has real roots, x1 = %.6f,  x2 = %.6f", x1, x2);
+        }
+           else if (q<0) {
+
+
+            realPart = (-b / (2 * a));
+            imgPart = (Math.sqrt(-q)) / (2 * a);
+
+            System.out.printf("The equation has complex roots, x1 = %.6f + %.6fi,  x2 = %.6f - %.6fi", realPart, imgPart, realPart, imgPart);
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
+
